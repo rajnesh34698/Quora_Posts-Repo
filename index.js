5ee42e26bcf4a002797f0617351d2bba439f0fbc
Rajnesh <rajnesh34698@gmail.com>
2023-12-16 11:44:22 +0530

    commiting complete project

---
+++ index.js
@@ -0,0 +1,85 @@
+const express=require("express");
+const app=express();
+const port=8080;
+app.set("view engine","ejs");
+const path=require("path");
+app.set("views",path.join(__dirname,"/views"));
+app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+const { v4: uuidv4 } = require('uuid');
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+let posts=[
+    {
+        id:uuidv4(),
+        username:"apna college",
+        content:"i love coding"
+    },
+    {
+        id:uuidv4(),
+        username:"shradha khapra",
+        content:"hard work is important to achieve success"
+    },
+    {
+        id:uuidv4(),
+        username:"rahul kumar",
+        content:"i got selected for my 1st internship"
+    }
+]
+
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
+});
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs")
+});
+app.post("/posts",(req,res)=>{
+    let {username,content}=req.body;
+    let id=uuidv4();
+    posts.push({id,username,content});
+    res.redirect("/posts");
+});
+app.get("/posts/:id",(req,res)=>{
+    let{id}=req.params;
+    let post =posts.find((p)=>id===p.id);
+    res.render("show.ejs",{post});
+});
+app.patch("/posts/:id",(req,res)=>{
+    let{id}=req.params;
+    let newContent=req.body.content;
+    let post=posts.find((p)=>id===p.id);
+    post.content=newContent;    
+    res.redirect("/posts");
+});
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id}=req.params;
+    let post=posts.find((p)=>id===p.id);
+    res.render("edit.ejs",{post});
+});
+app.delete("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    posts=posts.filter((p)=>id!==p.id);
+    res.redirect("/posts");
+})
+app.listen(port,()=>{
+    console.log(`listening to  the port ${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
